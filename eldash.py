fdbb771fc4178bdced5089f10b422098d04c8f4e
Juan Blanco <44730431+juansbg@users.noreply.github.com>
2021-11-26 14:33:11 +0100

    Dash Update 1

--- eldash.py
+++ eldash.py
@@ -3,72 +3,111 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+#import dash_core_components as dcc
+#import dash_html_components as html
+from dash import html
+from dash import dcc
 from dash.dependencies import Input, Output, State
 import logging
 from plotly.subplots import make_subplots
+import plotly.express as px
+import base64
+import plotly.io as pio
+
+# naming a layout theme for future reference
+pio.templates["google"] = go.layout.Template(
+    layout_colorway=['#4285F4', '#DB4437', '#F4B400', '#0F9D58',
+                     '#185ABC', '#B31412', '#EA8600', '#137333',
+                     '#d2e3fc', '#ceead6']
+)
+
+# setting Google color palette as default
+pio.templates.default = "google"
 
 # Para este Dash, vamos a seleccionar un fichero de datos y realizar un dashboard descriptivo
 # sobre un conjunto de datos
 
-df = pd.read_csv("StudentsPerformance.csv")
-
-# Crear opciones para las razas
-races = df["race/ethnicity"].unique().tolist()
-races.sort()
-options_dropdown_race = []
-for race in races:
-    options_dropdown_race.append({'label': race, 'value': race})
-
-# Crear opciones para las asignaturas
-subjects = ["math score", "reading score", "writing score"]
-options_dropdown_subjects = []
-for subject in subjects:
-    options_dropdown_subjects.append({'label': subject.split()[0].capitalize(), 'value': subject})
-
-# Crear opciones para las variables categóricas
-
-cols_checklist = ["gender","race/ethnicity","parental level of education", "lunch", "test preparation course"]
-
-options_checklist = []
-for col in cols_checklist:
-    options_checklist.append({'value': col, 'label': col})
+#df = pd.read_csv("StudentsPerformance.csv")
+df = pd.read_parquet("partitioned/").drop(columns=['partition'])
 
 app = dash.Dash()
 server = app.server
 
 #app.config.suppress_callback_exceptions = True
+app.css.append_css({'external_url': '/resources/style.css'})
+#app.server.static_folder = 'static'
 
 logging.getLogger('werkzeug').setLevel(logging.INFO)
 
+diccionario_terminos = {
+    'Total Transferido'  :'amount',
+    'Balance Old Origen' :'oldbalanceOrg',
+    'Balance New Origen' :'newbalanceOrig',
+    'Balance Old Destino':'oldbalanceDest',
+    'Balance New Destino':'newbalanceDest',
+    'Cambio en Origen'   :'cambioOrigen',
+    'Cambio en Destino'  :'cambioDestino'
+}
+diccionario_color_tipos = {
+    'CASH_OUT':'Con Fraude',
+    'CASH_IN' :'Sin Fraude',
+    'TRANSFER':'Con Fraude',
+    'PAYMENT' :'Sin Fraude',
+    'DEBIT'   :'Sin Fraude',
+    None:'red'
+}
+
+#encoded_image = base64.b64encode(open('assets/search.svg', 'rb').read()).decode()
 
 app.layout = html.Div([
-    html.H1( # Primera fila
-            children = [
-                "Introducción a Dash"
-            ],
-            id = "titulo",
-            style = {  # Aquí aplico todo lo que necesite de CSS
-                "text-align": "center", # Alineo el texto al centro
-                "color": "lightsteelblue", # Cambio el color de la fuente, se puede usar codigo hexagesimal
-                "font-family": "Arial", # Cambio el tipo de fuente
-                "backgroundColor": "darkslategray", # Cambio el color del fondo
-                "text-decoration": "underline" # Subrayar el texto
-            }
+    html.Div(
+        [
+            html.H1( # Primera fila
+                children = [
+                    'Detección de Fraudes'
+                    #"""
+                ],
+                id = "titulo",
+                style = {  # Aquí aplico todo lo que necesite de CSS
+                    "text-align": "center", # Alineo el texto al centro
+                    "font-size": "50px",
+                    "-webkit-text-fill-color": "transparent",
+                    "text-fill-color": "transparent",
+                    "-webkit-background-clip": "text",
+                    "background-clip": "text",
+                    "background-image": "linear-gradient(90deg,#58d68d,#f4d03f,#ec7063)"
+                }
+            )
+        ],
+        style={
+            "width":'600px',
+            "margin":"auto",
+        }
     ),
-
-    dcc.Tabs(id="tabs-styled-with-props", value='tab-1', children=[
-        dcc.Tab(label='Exploratorio', value='tab-1'),
-        dcc.Tab(label='Resultados modelo', value='tab-2'),
-        dcc.Tab(label='Consumición del modelo', value='tab-3')
-    ], colors={
-        "border": "white",
-        "primary": "gold",
-        "background": "cornsilk"
-    }),
-    html.Div(id='tabs-content-props')
-])
+    html.Div(
+        dcc.Tabs(id="tabs-styled-with-props", value='tab-1', children=[
+            dcc.Tab(label='Exploratorio', value='tab-1'),
+            dcc.Tab(label='Resultados modelo', value='tab-2'),
+            dcc.Tab(label='Consumición del modelo', value='tab-3')
+        ], colors={
+            "primary": "#58d68d",
+            #"background": "#f7dc6f",
+        }),
+        style={
+            "font-weight": "bold",
+            "font-size":"30px",
+            "background-image": "linear-gradient(90deg,#58d68d,#f4d03f,#ec7063)",
+            "color":"#323232",
+        }
+    ),
+    html.Div(id='tabs-content-props', style={"width":"95%","margin":"auto"})
+],
+    style={
+        "font-family":'"Century Gothic", CenturyGothic, Geneva, AppleGothic, sans-serif',
+        "background-color": "#323232",
+        "color":"white",
+    }
+)
 
 @app.callback(Output('tabs-content-props', 'children'),
               Input('tabs-styled-with-props', 'value'))
@@ -79,60 +118,41 @@ def render_content(tab):
             html.Div([
                 html.H3(
                     children = [
-                        "Primer grupo a comparar"
+                        "Naturaleza de las operaciones"
                     ],
-                    id = "primer_grupo",
+                    id = "titulo_1",
                     style = {
                         "display": "block",
                         "text-align": "center"
                     }
                 ),
-                dcc.Dropdown(
-                    options = options_dropdown_race,
-                    placeholder = "Selecciona una raza",
-                    id = "dropdown_race",
-                    style = {
-                        "display": "block",
-                        "width": "300px",
-                        "margin-left": "10px"
-                    }
+                html.P("Seleccionar Distribución:"),
+                dcc.RadioItems(
+                    id='tipoBoxplot', 
+                    options=[{'value': x, 'label': x} 
+                            for x in ['Total Transferido', 'Balance Old Origen','Balance New Origen','Balance Old Destino','Balance New Destino','Cambio en Origen', 'Cambio en Destino']],
+                    value='Total Transferido', 
+                    labelStyle={'display': 'inline-block'}
                 ),
-                dcc.Dropdown(
-                    options = options_dropdown_subjects,
-                    placeholder = "Selecciona una asignatura",
-                    id = "dropdown_subject",
-                    style = {
-                        "display": "block",
-                        "width": "300px",
-                        "margin-left": "10px"
-                    }
-                ),
-                dcc.Graph(
-                    id = "dropdown_figure",
-                    style = {
-                        "display": "none"
-                    }
-                )
+                dcc.Graph(id="box-plot"),
             ],
             style={
                     "width": "50%",
-                    "height": "575px",
+                    "height": "800px",
                     "text-align": "center",
                     "display": "inline-block",
                     "float":"left",
-                    "border-style": "ridge",
-                    "border-color": "fuchsia"
+                    #"border-style": "ridge",
+                    #"border-color": "fuchsia"
                 }
             ),
             
         ],
         style = {
             "width": "100%",
-            "height": "575px",
+            "height": "1000px",
             "text-align": "center",
-            "display": "inline-block",
-            "border-style": "ridge",
-            "border-color": "black"
+            "display": "inline-block"
         },
     )
     elif tab == 'tab-2':
@@ -144,169 +164,30 @@ def render_content(tab):
             html.H3('Esto es la Consumición del modelo')
         ])
 
+# BOX PLOT
 @app.callback(
-    Output("dropdown_figure", "figure"),
-    Output("dropdown_figure", "style"),
-    Input("dropdown_race", "value"),
-    Input("dropdown_subject",'value')
-)
-def figure_dropdown(dropdown_race_value,dropdown_subject_value):
+    Output("box-plot", "figure"), 
+    [Input("tipoBoxplot", "value")])
+def generate_chart(x):
+    data = df.sample(40000)
+    data = data[data[diccionario_terminos[x]] != 0]
+    fig = px.box(data, x=diccionario_terminos[x], y='type', color='isFraud', points="all", log_x=True, color_discrete_sequence=['#58d68d','#FFC300'],
+                    labels={
+                        "type": "Tipo de Transacción Realizada",
+                        diccionario_terminos[x]: x,
+                        "isFraud":"Presencia de Fraude",
+                    }
+                 )
+    #fig.update(layout_showlegend=False)
+    fig.update_layout(title='Boxplot Tipos de Transacciones', height=700, 
+                      paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)',
+                      font_color='white', 
+                      #title_font_color='white',legend_font_color='white',
+                      #legend_title_font_color='white',
+                      )
+    return fig
 
-    subject_dict = {
-        "math score": "Math",
-        "reading score": "Reading",
-        "writing score": "Writing"
-    }
-
-    if dropdown_race_value and dropdown_subject_value:
-        fig = go.Figure()
-        fig.add_trace(
-            go.Histogram(
-                x = df[df["race/ethnicity"] == dropdown_race_value][dropdown_subject_value],
-                xbins=dict(
-                    start= 0,
-                    end= 100,
-                    size=5,
-                ),
-                marker_color = "firebrick"
-            )
-        )
-        fig.update_layout(
-            title = f"Notas obtenidas en la asignatura {subject_dict[dropdown_subject_value]} de la raza {dropdown_race_value}",
-            xaxis_title = "Puntuación",
-            yaxis_title = "Frecuencia",
-            bargap = 0.1
-        )
-        return (fig,{"display":"block"})
-    else:
-        return (go.Figure(data = [], layout = {}), {"display": "none"})
-
-@app.callback(
-    Output("dropdown_figure_2", "figure"),
-    Output("dropdown_figure_2", "style"),
-    Input("dropdown_race_2", "value"),
-    Input("dropdown_subject_2",'value')
-)
-def figure_dropdown_2(dropdown_race_value,dropdown_subject_value):
-
-    subject_dict = {
-        "math score": "Math",
-        "reading score": "Reading",
-        "writing score": "Writing"
-    }
-
-    if dropdown_race_value and dropdown_subject_value:
-        fig = go.Figure()
-        fig.add_trace(
-            go.Histogram(
-                x = df[df["race/ethnicity"] == dropdown_race_value][dropdown_subject_value],
-                xbins=dict(
-                    start= 0,
-                    end= 100,
-                    size=5,
-                ),
-                marker_color = "steelblue"
-            )
-        )
-        fig.update_layout(
-            title = f"Notas obtenidas en la asignatura {subject_dict[dropdown_subject_value]} de la raza {dropdown_race_value}",
-            xaxis_title = "Puntuación",
-            yaxis_title = "Frecuencia",
-            bargap = 0.1
-        )
-        return (fig,{"display":"block"})
-    else:
-        return (go.Figure(data = [], layout = {}), {"display": "none"})
-
-@app.callback(
-    Output("pie_charts", "figure"),
-    Output("pie_charts", "style"),
-    Input("boton_cat", "n_clicks"),
-    State("checklist_cat", "value"),
-)
-def checklist_callback(n_clicks,checklist_value):
-    print(checklist_value)
-
-    if (checklist_value is None) or (checklist_value == []):
-        return (go.Figure(data = [], layout = {}), {"display":"none"})
-    else:
-        print(checklist_value)
-        fig = make_subplots(
-            rows=1,
-            cols=len(checklist_value),
-            specs = [[{'type':'domain'}]*len(checklist_value)]
-        )
-
-        for col in checklist_value:
-            level_count = pd.DataFrame(df[col].value_counts()).reset_index().rename(columns = {"index": col, col: "count"})
-            fig.add_trace(
-                go.Pie(
-                    labels = level_count[col],
-                    values = level_count["count"],
-                    textinfo = 'label+percent',
-                    insidetextorientation = 'radial',
-                    textposition = 'inside',
-                    sort = False,
-                    showlegend = False,
-                    hole = 0.5,
-                    name = col
-                ),
-                row = 1,
-                col = checklist_value.index(col) + 1
-            )
-
-        fig.update_traces(textfont_size=12)
-        return (fig,{"display": "block"})
-
+# OTROS
 
 if __name__ == '__main__':
     app.run_server()
-
-#     children=[
-#             html.H3(
-#                 children=[
-#                     "Segundo grupo a comparar"
-#                 ],
-#                 id="segundo_grupo",
-#                 style={
-#                     "display": "block",
-#                     "text-align": "center"
-#                 }
-#             ),
-#             dcc.Dropdown(
-#                 options=options_dropdown_race,
-#                 placeholder="Selecciona una raza",
-#                 id="dropdown_race_2",
-#                 style={
-#                     "display": "block",
-#                     "width": "300px",
-#                     "margin-left": "10px"
-#                 }
-#             ),
-#             dcc.Dropdown(
-#                 options=options_dropdown_subjects,
-#                 placeholder="Selecciona una asignatura",
-#                 id="dropdown_subject_2",
-#                 style={
-#                     "display": "block",
-#                     "width": "300px",
-#                     "margin-left": "10px"
-#                 }
-#             ),
-#             dcc.Graph(
-#                 id="dropdown_figure_2",
-#                 style={
-#                     "display": "none"
-#                 }
-#             )
-#     ],
-#         style={
-#             "width": "700px",
-#             "height": "575px",
-#             "display": "inline-block",
-#             "margin-left": "20px",
-#             "border-style": "ridge",
-#             "border-color": "black"
-#     },
-# )
-# ],
